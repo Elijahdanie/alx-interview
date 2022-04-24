60e826a345f78c0a2f85718b4483fc8fa86e86c7
Elijah Daniel <thachromatone@gmail.com>
2022-04-24 18:12:09 +0100

    used an entirely new and optimal approach

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,16 +12,13 @@ def minOperations(n):
     This returns the minimum operation to copy
     and paste n number of letters
     """
-    if n <= 0:
-        return 0
-    arr = []
-    iterator = int(n/2) if n > 10 else n
-    for i in range(iterator):
-        for j in range(iterator):
-            if (i * j) == n:
-                arr.append(i + j)
-    if len(arr) != 0:
-        arr.sort()
-        return arr[0]
-    else:
-        return n
+    track_operations = 0
+    max_operations = 2
+    while n > 1:
+        while n % max_operations == 0:
+            track_operations += max_operations
+            n /= max_operations
+        max_operations += 1
+    return track_operations
+
+print(minOperations(19170307))
