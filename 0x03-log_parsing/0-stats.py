91274b185b971a2765fc868de45566d600f51427
Elijahdanie <elijahdanie@gmail.com>
2022-04-29 03:16:18 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,28 +20,40 @@ def parseline(line):
         return None
 
 
+def print_stats(fileSize, map_):
+    print('File Size: {}'.format(fileSize))
+    [print('{}: {}'.format(i[0], i[1])) for i in map_.items()]
+    map_ = {}
+    fileSize = 0
+    aggregate = 0
+
+
 if __name__ == '__main__':
     """
     Entry point for the code
     """
     aggregate = 0
     fileSize = 0
-    code_map = {}
-    for line in fileinput.input():
-        line = line.strip()
-        parsed = parseline(line)
-        if parsed is not None:
-            aggregate += 1
-            fileSize += parsed['fileSize']
-            code_key = parsed['code']
-            if code_key in code_map.keys():
-                code_map[code_key] += 1
-            else:
-                code_map[code_key] = 1
-            code_map = dict(sorted(code_map.items()))
-            if aggregate == 10:
-                print('File Size: {}'.format(fileSize))
-                [print('{}: {}'.format(i[0], i[1])) for i in code_map.items()]
-                code_map = {}
-                fileSize = 0
-                aggregate = 0
+    map_ = {}
+    try:
+        for line in fileinput.input():
+            line = line.strip()
+            parsed = parseline(line)
+            if parsed is not None:
+                aggregate += 1
+                fileSize += parsed['fileSize']
+                code_key = parsed['code']
+                if code_key in map_.keys():
+                    map_[code_key] += 1
+                else:
+                    map_[code_key] = 1
+                map_ = dict(sorted(map_.items()))
+                if aggregate == 10:
+                    print('File Size: {}'.format(fileSize))
+                    [print('{}: {}'.format(i[0], i[1])) for i in map_.items()]
+                    map_ = {}
+                    fileSize = 0
+                    aggregate = 0
+    except KeyboardInterrupt:
+        print_stats(fileSize, map_)
+        raise
