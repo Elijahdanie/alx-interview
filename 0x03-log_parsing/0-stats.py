ffdf5c8873b1c7226c8f17d1deae180a39e0a68c
Elijahdanie <elijahdanie@gmail.com>
2022-04-29 02:56:24 +0100

    modifications

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,22 +25,22 @@ def parseline(line):
     except Exception as e:
         return None
 
-
-for line in fileinput.input():
-    line = line.strip()
-    parsed = parseline(line)
-    if parsed is not None:
-        aggregate += 1
-        fileSize += parsed['fileSize']
-        code_key = parsed['code']
-        if code_key in code_map.keys():
-            code_map[code_key] += 1
-        else:
-            code_map[code_key] = 1
-        code_map = dict(sorted(code_map.items()))
-        if aggregate == 10:
-            print('File Size: {}'.format(fileSize))
-            [print('{}: {}'.format(i[0], i[1])) for i in code_map.items()]
-            code_map = {}
-            fileSize = 0
-            aggregate = 0
+if __name__ == '__main__':
+    for line in fileinput.input():
+        line = line.strip()
+        parsed = parseline(line)
+        if parsed is not None:
+            aggregate += 1
+            fileSize += parsed['fileSize']
+            code_key = parsed['code']
+            if code_key in code_map.keys():
+                code_map[code_key] += 1
+            else:
+                code_map[code_key] = 1
+            code_map = dict(sorted(code_map.items()))
+            if aggregate == 10:
+                print('File Size: {}'.format(fileSize))
+                [print('{}: {}'.format(i[0], i[1])) for i in code_map.items()]
+                code_map = {}
+                fileSize = 0
+                aggregate = 0
