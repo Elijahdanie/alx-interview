74889d9256e5a32f4d4b2cf7d5551a37d8a6bcef
Elijah Daniel <thachromatone@gmail.com>
2022-05-20 00:51:27 +0100

    update

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const req = require("reuqest");
+
+const baseUrl = "https://swapi-api.hbtn.io/api/films";
+
+const id = process.argv[2];
+var names = []
+var len = 0;
+var tempDict = {}
+
+if (id) {
+   req.get(`${baseUrl}/${id}/`, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const data = JSON.parse(body);
+      const characters = data.characters;
+      len = characters.length;
+      names = characters;
+      characters.forEach((element) => {
+        req.get(element, (err, res, body) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const data = JSON.parse(body);
+            track(data.name, data.url);
+          }
+        });
+      });
+    }
+  });
+}
+
+function track(n, url)
+{
+    len -=1;
+    tempDict[url] = n;
+    if (len == 0)
+    {
+        names.forEach(name =>{
+            console.log(tempDict[name]);
+        })
+    }
+}
