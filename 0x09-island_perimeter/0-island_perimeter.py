f530cacc40bc6fdc864b3759aeeaf07c937c427b
Elijah Daniel <thachromatone@gmail.com>
2022-06-10 04:27:47 +0100

    update

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+This module computes the perimeter
+of an island in a grid
+"""
+
+def island_perimeter(grid):
+    """
+    This function computes the perimeter
+    of an island in a grid
+    """
+    c_p = 0
+    len_grid = len(grid)
+    for i in range(len_grid):
+        row = grid[i]
+        row_len = len(row)
+        for cell in range(row_len):
+            if row[cell] == 1:
+                bool_map = []
+                bool_map.append(row[cell - 1] == 0 or cell - 1 < 0)
+                bool_map.append(row[cell + 1] == 0 or cell + 1 > row_len - 1)
+                bool_map.append(grid[i - 1][cell] == 0 or i - 1 < 0)
+                bool_map.append(grid[i + 1][cell] == 0 or i + 1 > len_grid - 1)
+                for i in bool_map:
+                    if i == True:
+                        c_p += 1
+    return c_p
