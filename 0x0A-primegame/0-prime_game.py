369b7c351e548967ef52489a25a9d98a40beb956
Elijah Daniel <thachromatone@gmail.com>
2022-06-16 12:59:30 +0100

    check this

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,86 +1,44 @@
 #!/usr/bin/python3
+"""
+Define isWineer function, a solution to the Prime Game problem
+"""
 
-"""
-This module computes the
-winner of the prime game
-"""
+
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
 
 
 def isWinner(x, nums):
     """
-    Entry point of the computation
+    Determines winner of Prime Game
+    Args:
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
     """
     if x is None or nums is None or x == 0 or nums == []:
         return None
-    contestant = {'Maria': 0, 'Ben': 0}
+    Maria = Ben = 0
     for i in range(x):
-        if(i < len(nums)):
-            game_map = list(range(1, nums[i]))
-            winner = play(game_map)
-            if winner:
-                contestant[winner] += 1
-    mar_ = contestant['Maria']
-    beb_ = contestant['Ben']
-    if mar_ > beb_:
-        return 'Maria'
-    elif beb_ > mar_:
-        return 'Ben'
-    else:
-        return None
-
-
-def play(game_map: list):
-    """
-    This function represent a round for the game
-    """
-    curr_player = 0
-    while(len(game_map) > 0):
-        game_map = pick(game_map)
-        curr_player = curr_player + 1
-        if curr_player > 1:
-            curr_player = 0
-    if (game_map == []):
-        return 'Maria' if curr_player == 0 else 'Ben'
-
-
-def pick(map: list):
-    """
-    This function picks a prime number
-    from the list and removes its multiples
-    """
-    num = getprime(map)
-    if num is None:
-        return []
-    max_ = map[-1] if len(map) > 1 else map[0]
-    map.remove(num)
-    while num < max_:
-        num = num * num
-        if num in map:
-            map.remove(num)
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
+            Ben += 1
         else:
-            return map
-    return map
-
-
-def getprime(map):
-    """
-    This function picks the next
-    prime number from the list
-    """
-    for i in range(len(map)):
-        if isprime(map[i]):
-            return map[i]
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
     return None
-
-
-def isprime(num):
-    """
-    This function checks if number is a
-    prime number
-    """
-    if(num == 1):
-        return False
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
